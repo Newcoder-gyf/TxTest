3558a1f8393ed2af2213ced4c2aa970bc0278396
YangfeiGao <gyf112358@hotmail.com>
2018-03-05 20:21:22 -0500

    update

---
+++ src/Day07/08TemplateDemo01.java
@@ -0,0 +1,17 @@
+package Day07;
+/*
+* 抽象模版
+*
+*
+* */
+abstract class AbstractTemplate01 {
+    /*
+    * 必须要实现的业务方法
+    * */
+    public abstract void abstractMethod01();
+
+    /*
+    * 定义一个可以重写也可以不重写的方法
+    *
+    * */
+}
