d113226c1917e40044997dbe7a5921bd43a44e9a
YangfeiGao <gyf112358@hotmail.com>
2018-03-19 15:07:00 -0400

    new template
    new template

--- src/Day07/08TemplateDemo01.java
+++ src/Day07/08TemplateDemo01.java
@@ -1,10 +1,11 @@
 package Day07;
 /*
 * 抽象模版
-*
+* （里面有三个方法 ）
 *
 * */
 abstract class AbstractTemplate01 {
+
     /*
     * 必须要实现的业务方法
     * */
@@ -12,6 +13,23 @@ abstract class AbstractTemplate01 {
 
     /*
     * 定义一个可以重写也可以不重写的方法
-    *
     * */
+    public void hookMethod(){};
+
+    //定义一个必须不能被重写的方法
+    public final void concreatMethod(){}
+
+    //模版方法
+    public void templateMethod(){
+        hookMethod();
+        abstractMethod01();
+        concreatMethod();
+    }
+
+    /*
+    * 具体的模版
+    * */
+
+
+
 }
