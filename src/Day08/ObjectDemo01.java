68ebc8f83c7bb2887fd10c6cc513459d598fed02
YangfeiGao <gyf112358@hotmail.com>
2018-04-16 17:29:50 -0400

    object method practice
    toString
    Hashcode

--- src/Day08/ObjectDemo01.java
+++ src/Day08/ObjectDemo01.java
@@ -1,5 +1,8 @@
 package Day08;
-
+/*
+* java.long.Object 是被每一个类默认继承的
+*
+* */
 class Student{
     private String name;
     private int age;
@@ -27,10 +30,23 @@ class Student{
         return gender;
     }
 
+    public String toString(){
+        return this.name +" age is: "+ this.age +" gender is: "+ this.gender;
+    }
+
 }
 
 class ObjectDemo01{
     public static void main(String[] args){
         Student st = new Student();
+        st.setName("Yang");
+        st.setAge(23);
+        st.setGender(1);
+        //使用学生对象继承父类Object下的toString 方法
+        String str = st.toString();
+        System.out.println("HexHashcode is " + str);
+        int strhash = st.hashCode();
+        //使用学生对象继承父类Object下的hashcode方法
+        System.out.println("Student's hashcode is "+ strhash);
     }
 }
