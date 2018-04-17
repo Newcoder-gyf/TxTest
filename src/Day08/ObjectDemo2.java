cbdb78e82eba9e30576346e609e716fc1d201ce3
YangfeiGao <gyf112358@hotmail.com>
2018-04-17 17:04:32 -0400

    object equals test

--- src/Day08/ObjectDemo2.java
+++ src/Day08/ObjectDemo2.java
@@ -32,6 +32,13 @@ class ObjectDemo02{
     public static void main(String[] args){
         Student02 st01 = new Student02();
         Student02 st02 = new Student02();
+        //print 输出st01 默认调用object类下的toString方法
         System.out.println(st01);
+        System.out.println(st02);
+
+        //判断对象是否相等不能使用"=="， 因为"==" 是判断两个对象的地址，而两个对象的地址肯定不相等
+        System.out.println(st01 == st02);
+
+        //判断两个对象是否相等，应该使用"equals"方法 
     }
 }
