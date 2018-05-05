78e0dba8fdcebaec86085a7fb2e4d125dc898a08
YangfeiGao <gyf112358@hotmail.com>
2018-05-04 20:11:40 -0400

    string test

---
+++ src/Day11/StringDemo07.java
@@ -0,0 +1,12 @@
+package Day11;
+
+public class StringDemo07 {
+    public static void main(String[] args) {
+        String str = "helloworld";
+        //从指定索引处查找字符出现的索引(包含起始索引本身)
+        int index = str.indexOf('l', 2);
+        System.out.println(index);
+
+        //获取字符串的子字符串
+    }
+}
