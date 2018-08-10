b0932674a836c5e443208133258569e9c0423a56
YangfeiGao <gyf112358@hotmail.com>
2018-08-10 12:34:08 -0400

    time

--- src/Day11/StringDemo08.java
+++ src/Day11/StringDemo08.java
@@ -5,20 +5,6 @@ import java.util.Scanner;
 public class StringDemo08 {
     public static void main(String[] args){
 
-//        String testStr01 = "testjava";
-//        System.out.println(testStr01.toUpperCase());
-//
-//        String testStr02 = "TESTJAVA";
-//        System.out.println(testStr02.toLowerCase());
-//        System.out.println(testStr01 + "+" + testStr02);
-//        System.out.println(testStr01.concat("+" +testStr02));
-//
-//        String testStr03 = "hello world";
-//        String testHead = testStr03.substring(0,1);
-//        System.out.println(testHead);
-//        String appendHead = testHead.toUpperCase();
-//        String testTail = testStr03.substring(1);
-//        System.out.println(appendHead + testTail);
         callMethod();
 
     }
