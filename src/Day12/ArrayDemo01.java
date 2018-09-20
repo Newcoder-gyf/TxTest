5cc23cff9a1bb8b8daf226c365e7b8709a9bfa34
YangfeiGao <gyf112358@hotmail.com>
2018-09-19 23:12:25 -0400

    collection

--- src/Day12/ArrayDemo01.java
+++ src/Day12/ArrayDemo01.java
@@ -1,4 +1,40 @@
 package Day12;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class ArrayDemo01 {
+
+    public static void main(String[] args) throws ParseException {
+
+        String str = "1999-10-10";
+        SimpleDateFormat newDateFormat = new SimpleDateFormat("yyyy-mm-dd");
+        Date date =  newDateFormat.parse(str);
+
+        //定义一个学生数组
+        //数组作为集合是有局限性的，数组长度是固定的
+        Student[] studentsCollection = new Student[10];
+
+        for (int i = 0; i < 10; i ++ ){
+            Student s = new Student(i, "gyf"+i,  i%2,  new Date());
+            //System.out.println(s);
+            studentsCollection[i] = s;
+        }
+        for (int i = 0; i < studentsCollection.length; i ++){
+            System.out.println(studentsCollection[i]);
+        }
+
+        System.out.println("-------------------------");
+
+        Object[] objs = new Object[10];
+        //objs[0] = 1;
+        objs[1] = 2;
+        objs[2] = "hehe";
+        objs[3] = 'a';
+        for (int i = 0; i < objs.length; i ++){
+            System.out.println(objs[i]);
+        }
+
+    }
 }
--- src/Day12/Student.java
+++ src/Day12/Student.java
@@ -1,6 +1,59 @@
 package Day12;
 
+import java.util.Date;
+
 public class Student {
     private Integer id;
-    
+    private String name;
+    private Integer gender;
+    private Date birthday;
+
+    public Student(Integer id, String name, Integer gender, Date birthday) {
+        this.id = id;
+        this.name = name;
+        this.gender = gender;
+        this.birthday = birthday;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", gender=" + gender +
+                ", birthday=" + birthday +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
 }
