7ed9a09e6e9a6e71171c16f3d02266a699468313
YangfeiGao <gyf112358@hotmail.com>
2018-09-27 20:54:47 -0400

    advanced forloop

--- src/Day12/AdvancedForLoopDemo.java
+++ src/Day12/AdvancedForLoopDemo.java
@@ -1,8 +1,10 @@
 package Day12;
 
 import java.util.ArrayList;
+import java.util.Date;
 
 //增强的for循环
+//此for循环不是线程安全的
 public class AdvancedForLoopDemo {
 
     public static void main(String[] args) {
@@ -10,6 +12,21 @@ public class AdvancedForLoopDemo {
         ArrayList<String> newArrayList01 = new ArrayList<String>();
         for (int i = 0; i < 5; i++){
             newArrayList01.add("test0" + i);
+        }
+
+        for(String element: newArrayList01){
+            System.out.println(element);
+        }
+
+        ArrayList<Student> newStudentArrayList01 = new ArrayList<Student>();
+        for (int i = 0; i < 5; i ++){
+            newStudentArrayList01.add(new Student(i, "Gao" + i, i%2, new Date()));
+        }
+        System.out.println(newStudentArrayList01);
+
+        //非线程安全
+        for(Student element: newStudentArrayList01){
+            System.out.println(element);
         }
     }
 }
