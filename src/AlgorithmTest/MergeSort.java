70d0f34a20a1258cf971fd99cae91438e9f4681b
YangfeiGao <gyf112358@hotmail.com>
2018-12-21 18:18:27 -0500

    test for sort

---
+++ src/AlgorithmTest/MergeSort.java
@@ -0,0 +1,15 @@
+package AlgorithmTest;
+
+import java.util.Arrays;
+
+public class MergeSort {
+
+    public static void main(String[] args){
+        int[] sequence = new int[] {9, 1, 5, 8, 3, 7, 4, 6, 2};
+        System.out.println("Before sorting: " + Arrays.toString(sequence));
+        System.out.println("After sorting: " + Arrays.toString(sequence));
+    }
+
+    
+
+}
